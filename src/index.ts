cafe4140627c87dd5fe97634254a9d4575eb6774
Afdhalul Ihsan Rosyidi <ihsan.r28@gmail.com>
2020-11-12 00:09:32 +0700

    fix production

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-import 'reflect-metadata';
-require('dotenv').config();
+import "reflect-metadata";
+require("dotenv").config();
 
-import { createConnection } from 'typeorm';
-import * as express from 'express';
-import { Response, Request } from 'express';
-import * as bodyParser from 'body-parser';
-import * as helmet from 'helmet';
-import * as cors from 'cors';
+import {createConnection} from "typeorm";
+import * as express from "express";
+import {Response, Request} from "express";
+import * as bodyParser from "body-parser";
+import * as helmet from "helmet";
+import * as cors from "cors";
+import * as morgan from "morgan";
 
-import routes from './routes';
+import routes from "./routes";
 
 const port = process.env.PORT || 3000;
 const app: express.Application = express();
 
 //Connects to the Database -> then starts the express
 createConnection()
-    .then(async connection => {
-        //
-        // Implement middlewares
-        app.use(cors());
-        app.use(helmet());
-        app.use(bodyParser.json());
-        app.use(express.urlencoded({extended: true}));
+	.then(async connection => {
+		//
+		// Implement middlewares
+		app.use(cors());
+		app.use(helmet());
+		app.use(morgan("combined"));
+		app.use(bodyParser.json());
+		app.use(express.urlencoded({extended: true}));
 
-        // Sets all routes
-        app.use('/', routes);
+		// Sets all routes
+		app.use("/", routes);
 
-        // Start server
-        app.listen(port, () => {
-            console.log(`Server started on port http://localhost:${port}`);
-        });
-    })
-    .catch(error => console.log(error));
+		// Start server
+		app.listen(port, () => {
+			console.log(`Server started on port http://localhost:${port}`);
+		});
+	})
+	.catch(error => console.log(error));
